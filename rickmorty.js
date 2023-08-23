42d949d8b16e63f6ee4a66ba39eb5364d68dfbae
Thiago <124899004+umthiago@users.noreply.github.com>
2023-08-22 22:12:24 -0300

    Update rickmorty.js

--- rickmorty.js
+++ rickmorty.js
@@ -1,48 +1,43 @@
-function limparFormulario(endereco){
-    document.getElementById("endereco").value = ""
-    document.getElementById("bairro").value = ""
-    document.getElementById("cidade").value = ""
-    document.getElementById("estado").value = ""
-    document.getElementById("complemento").value = ""
-    document.getElementById("ddd").value = ""
-    document.getElementById("ibge").value = ""
-    document.getElementById("siafi").value = ""
-}
+const characterForm = document.getElementById('character-form');
+const characterInfo = document.getElementById('character-info');
+const characterImage = document.getElementById('character-image');
 
-function preencherFormulario(endereco) {
-    document.getElementById("endereco").value = endereco.logradouro
-    document.getElementById("bairro").value = endereco.bairro
-    document.getElementById("cidade").value = endereco.localidade
-    document.getElementById("estado").value = endereco.uf
-    document.getElementById("complemento").value = endereco.complemento
-    document.getElementById("ddd").value = endereco.ddd
-    document.getElementById("ibge").value = endereco.ibge
-    document.getElementById("siafi").value = endereco.siafi
-}
+characterForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const characterNameInput = document.getElementById('character-name');
+    const characterName = characterNameInput.value;
+    getCharacterInfo(characterName);
+});
 
-function eNumero(numero) {
-    return /^[0-9]+$/.test(numero)
+function getCharacterInfo(characterName) {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${characterName}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.results.length === 0) {
+                characterInfo.innerHTML = 'Character not found.';
+                characterImage.src = '';
+                characterInfo.style.display = 'block';
+                characterImage.style.display = 'none';
+            } else {
+                const character = data.results[0];
+                characterInfo.innerHTML = `
+                    <h2>${character.name}</h2>
+                    <p><strong>Status:</strong> ${character.status}</p>
+                    <p><strong>Species:</strong> ${character.species}</p>
+                    <p><strong>Gender:</strong> ${character.gender}</p>
+                    <p><strong>Origin:</strong> ${character.origin.name}</p>
+                    <p><strong>Location:</strong> ${character.location.name}</p>
+                `;
+                characterImage.src = character.image;
+                characterInfo.style.display = 'block';
+                characterImage.style.display = 'block';
+            }
+        })
+        .catch(error => {
+            characterInfo.innerHTML = 'An error occurred.';
+            characterImage.src = '';
+            characterInfo.style.display = 'block';
+            characterImage.style.display = 'none';
+            console.error('Error fetching data:', error);
+        });
 }
-
-function cepValid(cep) {
-    return cep.length == 8 && eNumero(cep)
-}
-
-async function pesquisarCep() {
-    limparFormulario(endereco)
-    const cep = document.getElementById("cep").value.replace("-", "")
-    console.log(cep)
-    const url = "https://rickandmortyapi.com/api/character/"
-    const dados = await fetch(url)
-    console.log(dados)
-    const personagens = await dados.json()
-    console.log(personagens)
-    // for (let i = 0; i < array.length; i++) {
-    //     if(personagens.id)
-        
-    // }
-    // console.log(personagens)
-}
-    
-
-document.getElementById("cep").addEventListener("focusout", pesquisarCep)
